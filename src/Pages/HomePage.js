da1ffd01b2791d213a1eb9910b4d6d243f79dbb9
santoshi-1 <karuturisantoshi@gmail.com>
2021-11-20 14:38:40 +0530

    added simple home page

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const HomePage = () => (
+    <React.Fragment>
+        <h1>Hello, Home Page</h1>
+        <p>This is a home page</p>
+    </React.Fragment>
+)
+
+export default HomePage
