6c6fa7de7ea0fd3cb1209872d94048f582c52ff2
santoshi-1 <karuturisantoshi@gmail.com>
2022-10-06 10:06:00 +0530

    changes related to new project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
-import HomePage from "./Pages/HomePage";
 
 function App() {
-	console.log("test");
+	
 	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<HomePage />} exact />
-			</Routes>
-		</Router>
+		<div className="App">
+			<h1>My Awesome Blog</h1>
+			<div id="page-body">Welcome to my blog!</div>
+		</div>
 	);
 }
 
